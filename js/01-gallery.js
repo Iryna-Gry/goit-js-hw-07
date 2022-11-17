a6567e5b203b6184e6549e99192a1d9884b8364a
Iryna Grytsaenko <iryna.grytsaenko@gmail.com>
2022-11-17 15:45:17 +0200

    Adds 1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,61 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryList = document.querySelector("div.gallery");
 
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+galleryList.innerHTML = createGalleryMarkup(galleryItems);
 console.log(galleryItems);
+
+const onGalleryImageClick = (evt) => {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  } else {
+    modalWindowOpen(evt);
+  }
+};
+function modalWindowOpen(evt) {
+  const instance = basicLightbox.create(
+    `
+    <div class="modal">
+        <img
+      class="gallery__image"
+      src=${evt.target.dataset.source}
+      alt=${evt.target.alt}
+    />
+    </div>
+`,
+    {
+      onShow: (instance) => {
+        instance.element().querySelector("div.modal").onclick = instance.close;
+        document.addEventListener(
+          "keydown",
+          (event) => {
+            if (event.key === "Escape") {
+              instance.close();
+            }
+          },
+          { once: true }
+        );
+      },
+    }
+  );
+  instance.show();
+}
+galleryList.addEventListener("click", onGalleryImageClick);
