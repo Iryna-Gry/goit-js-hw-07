7cc8a79ae1bc5aca7c2cffb36e40bc3ab31b5269
Iryna Grytsaenko <iryna.grytsaenko@gmail.com>
2022-11-17 17:15:42 +0200

    Adds 2 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -10,7 +10,8 @@ function createGalleryMarkup(galleryItems) {
   <a class="gallery__link" href=${original}>
     <img
       class="gallery__image"
-      src=${preview}
+      loading="lazy"
+      data-src=${preview}
       data-source=${original}
       alt=${description}
     />
@@ -57,5 +58,18 @@ function modalWindowOpen(evt) {
     }
   );
   instance.show();
+}
+if ("loading" in HTMLImageElement.prototype) {
+  const imgArray = document.querySelectorAll('img[loading = "lazy"]');
+  imgArray.forEach((img) => (img.src = img.dataset.src));
+} else {
+  const script = document.createElement("script");
+  script.src =
+    "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+  script.integrity =
+    "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
+  script.crossorigin = "anonymous";
+  script.referrerpolicy = "no-referrer";
+  document.body.appendChild(script);
 }
 galleryList.addEventListener("click", onGalleryImageClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
+const galleryList = document.querySelector("ul.gallery");
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+</a>`;
+    })
+    .join("");
+}
+galleryList.innerHTML = createGalleryMarkup(galleryItems);
 console.log(galleryItems);
+let gallery = new SimpleLightbox(".gallery a");
+gallery.on("show.simplelightbox", function () {
+  gallery.defaultOptions.captionsData = "alt";
+  gallery.defaultOptions.captionDelay = 250;
+});
+
+gallery.on("error.simplelightbox", function (e) {
+  console.log(e);
+});
