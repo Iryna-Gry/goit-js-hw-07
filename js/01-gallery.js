aeb7e94b01ed30cfda667120d4d895328e4b7ede
Iryna Grytsaenko <iryna.grytsaenko@gmail.com>
2022-11-18 11:20:49 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -78,10 +78,7 @@ if ("loading" in HTMLImageElement.prototype) {
   const script = document.createElement("script");
   script.src =
     "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
-  script.integrity =
-    "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
-  script.crossorigin = "anonymous";
-  script.referrerpolicy = "no-referrer";
+  script.type = "module";
   document.body.appendChild(script);
 }
 galleryList.addEventListener("click", onGalleryImageClick);
